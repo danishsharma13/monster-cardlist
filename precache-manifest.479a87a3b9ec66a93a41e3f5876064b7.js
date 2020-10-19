ff3f33e47a3ea54d5ac9856c02cfeb77832fa735
Danish Sharma <danishsharma1999@gmail.com>
2020-10-19 00:38:34 -0400

    Updates

---
+++ precache-manifest.479a87a3b9ec66a93a41e3f5876064b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9406eb3053b9b815cd2a02e7eb3d6b",
+    "url": "/monster-cardlist/index.html"
+  },
+  {
+    "revision": "652444bdbf743d3dc79f",
+    "url": "/monster-cardlist/static/css/main.37554e71.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/monster-cardlist/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-cardlist/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652444bdbf743d3dc79f",
+    "url": "/monster-cardlist/static/js/main.22b66b42.chunk.js"
+  },
+  {
+    "revision": "2d1b30dc96cfd4b43759",
+    "url": "/monster-cardlist/static/js/runtime-main.7496bf43.js"
+  }
+]);
